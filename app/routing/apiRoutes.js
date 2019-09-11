2428cd390d63074ce1dd1b5c49e53b12d3620a20
James Rudd <james.rudd45@gmail.com>
2019-09-11 12:40:25 +1000

    API GET request functioning

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,14 @@
 // Initialise the Express server and create the Routing meathod
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 // Call the data from the friends.js file
-var friends = require("../data/friends")
+var friends = require(path.join(__dirname, "../data/friends.js"));
 
-router.get("", function (req, res) {
+router.get("/friends", function (req, res) {
     res.json(friends)
-})
+});
+
+// Export the module for use in the server
+module.exports = router;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+// Initialise the Express server and create the Routing meathod
+var express = require('express');
+var router = express.Router();
+
+
+
+router.get("/survey", function (req, res) {
+
+});
+
+// Export the module for use in the server
+module.exports = router;
